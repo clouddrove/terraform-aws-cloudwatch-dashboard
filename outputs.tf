e334c20b7de35c722ecea414ea2b2d8dd84524f7
Anmol Nagpal <anmol@clouddrove.com>
2023-11-21 22:31:51 +0530

    feat: added example for ecr dashboard and did changes in readme

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,6 @@
 #Description : Terraform module creates Cloudwatch Dashboard on AWS for monitoriing AWS services.
 
 output "dashboard_arn" {
-  value = join("", aws_cloudwatch_dashboard.default[*].dashboard_arn)
+  description = "The Amazon Resource Name (ARN) of the dashboard"
+  value       = join("", aws_cloudwatch_dashboard.default[*].dashboard_arn)
 }
