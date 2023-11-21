4d64d31c3c01087ddcf28b80fa1a8b9e22138206
Himanshu Ahirwar <83774016+h1manshu98@users.noreply.github.com>
2023-11-21 22:07:43 +0530

    feat: cloudwatch dashboard resource with example (#6)
    * feat: cloudwatch dashboard resource with example
    
    * fix- tf-checks
    
    * fix- readme action

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
 #Module      : CLOUDWATCH DASHBOARD MODULE
 #Description : Terraform module creates Cloudwatch Dashboard on AWS for monitoriing AWS services.
-locals {
-  dashboard = {
-    start          = var.start
-    end            = var.end
-    periodOverride = var.periodOverride
-    widgets        = var.widgets
-  }
-  json_map = jsonencode(local.dashboard)
+
+##-----------------------------------------------------------------------------
+## Labels module callled that will be used for naming and tags.
+##-----------------------------------------------------------------------------
+module "labels" {
+  source      = "clouddrove/labels/aws"
+  version     = "1.3.0"
+  name        = var.name
+  repository  = var.repository
+  environment = var.environment
+  managedby   = var.managedby
+  label_order = var.label_order
 }
 
+resource "aws_cloudwatch_dashboard" "default" {
+  count          = var.enable ? 1 : 0
+  dashboard_name = module.labels.id
+  dashboard_body = var.dashboard_body
+}
